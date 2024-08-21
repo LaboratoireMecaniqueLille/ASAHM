78ea55e2bb1f4858f2b5a682109f2388c9753318
Antoine Weisrock <antoine.weisrock@gmail.com>
2024-08-21 16:02:17 +0200

    syntax: fix indentation in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,27 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import pathlib
+import sys
+
 project = 'ASAHM'
 copyright = '2023, Tejada Luis, Coulibaly Ibrahim'
 author = 'Tejada Luis, Coulibaly Ibrahim'
 release = '0.0.1'
 
-import pathlib
-import sys
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-	'sphinx.ext.autodoc',
-   	'sphinx.ext.autosummary',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
